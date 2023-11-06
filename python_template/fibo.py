40d6a0619b614d1c9d575bda5898e92ffad384b6
Tsuba-Nyan <tsu.sugi@gmail.com>
2023-11-06 22:08:58 +0900

    update: improve argument checks

--- python_template/fibo.py
+++ python_template/fibo.py
@@ -17,10 +17,21 @@ def fib2(n):
 
 if __name__ == "__main__":
     import sys
+    import re
 
+    # check the number of arguments
     if len(sys.argv) < 2:
         print("Usage: {0} number".format(sys.argv[0]))
-        sys.exit(102)
+        sys.exit(100)
 
-    fib(int(sys.argv[1]))
+    n = sys.argv[1]
+
+    # check if the string includes only digits
+    match = re.fullmatch('[0-9]+', n)
+    if not match:
+        print("needs only integer!")
+        print("Usage: {0} number".format(sys.argv[0]))
+        sys.exit(101)
+
+    fib(int(n))
     sys.exit(0)

48e9c82005cb1b42cfdc620870f5a8d0036ba572
Tsuba-Nyan <tsu.sugi@gmail.com>
2023-11-09 21:57:57 +0900

    update: modified the notice about this program. added an argument to set the max number of line. added feature reading until the max line number.

--- python_template/read_file.py
+++ python_template/read_file.py
@@ -8,13 +8,30 @@ if __name__ == "__main__":
         prog = 'read_file',
         description = 'Read each line from a input file')
 
-    # notice: below code does not allow to close the file automatically
-    parser.add_argument('infile', nargs=1, type=argparse.FileType("r"))
+    """ notice: Below code does not close the file automatically
+               when something unexpected has come up. But Python garbage 
+               collector destroy file descriptors after the running the 
+               program, so you don't worry about that usually.
+    """
+            
+    parser.add_argument('infile', nargs=1, type=argparse.FileType("r"),
+                        help='an input file to read')
+    parser.add_argument('--max_number', dest='max_num', default=30,
+                        type=int, 
+                        help='max line number the integers. '
+                            'this program reads the file until the number'
+                            'of line. Default value is 30.')
 
     args = parser.parse_args()
 
     f = args.infile[0]
+    max_num = args.max_num
+    i = 0
 
     for line in f:
+        if i >= max_num:
+            break
         print(line, end='')
+        i+=1
+
     f.close()

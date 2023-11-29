fd2fa164578fa66bbcdf325405d41a93cd0c80e0
Tsuba-Nyan <tsu.sugi@gmail.com>
2023-11-29 22:53:11 +0900

    update: improve regular expressions and add test-cases

--- python_template/sum_stdin.py
+++ python_template/sum_stdin.py
@@ -1,4 +1,55 @@
 #!/usr/bin/python
+import unittest
+import re
+
+def regex_signed_integer():
+    regex = '^[-+]?\d+$'
+    return(regex)
+
+def regex_signed_float():
+    regex = '^[-+]?\d+\.\d+$'
+    return(regex)
+
+def regex_expression():
+    regex = '^[-+]?\d+( *[-+] *\d+)+$'
+    return(regex)
+
+class TestRegularExpression(unittest.TestCase):
+    
+    def test_regex_signed_integer(self):
+        value_list = ['7', '39', '-5', '+4']
+        for value in value_list:
+            matched = re.search(regex_signed_integer(), value)
+            self.assertTrue(matched, True)
+
+        value_list = ['1 + 2', '3.4', '0.5', '-1.5', '+44.2', 'a', 'x2']
+        for value in value_list:
+            matched = re.search(regex_signed_integer(), value)
+            self.assertFalse(matched, True)
+        
+    def test_regex_signed_float(self):
+        value_list = ['7.2', '0.39', '-5.2', '+0.4']
+        for value in value_list:
+            matched = re.search(regex_signed_float(), value)
+            self.assertTrue(matched, True)
+
+        value_list = ['1 + 2', '34', '05', '-15', '+442', 'a', 'x2']
+        for value in value_list:
+            matched = re.search(regex_signed_float(), value)
+            self.assertFalse(matched, True)
+        
+    def test_regex_expression(self):
+        value_list = ['1 + 2', '5+9', '32 - 4']
+        for value in value_list:
+            matched = re.search(regex_expression(), value)
+            self.assertTrue(matched, True)
+
+        value_list = ['12', '3.4', '05', '-15', '+442', 'a', 'x2']
+        for value in value_list:
+            matched = re.search(regex_expression(), value)
+            self.assertFalse(matched, True)
+        
+
 
 if __name__ == "__main__":
     import sys
@@ -21,21 +72,21 @@ if __name__ == "__main__":
             index = IDX_VALUE_ONLY
 
         value = "".join(line[index:])
-        if matched := re.search('^[-+]?[\d]+$', value):
+        if matched := re.search(regex_signed_integer(), value):
             try:
                 num = int(value)
             except ValueError:
                 print(f"failed to convert to int. [{value}]",
                     file=sys.stderr)
                 sys.exit(False)
-        elif matched := re.search('^[-+]?[\d.]+$', value):
+        elif matched := re.search(regex_signed_float(), value):
             try:
                 num = float(value)
             except ValueError:
                 print(f"failed to convert to float. [{value}]",
                     file=sys.stderr)
                 sys.exit(False)
-        elif matched := re.search('^[\+\- ]*\d+[\+\- ]+\d+', value):
+        elif matched := re.search(regex_expression(), value):
             try:
                 num = eval(value)
             except NameError:

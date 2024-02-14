ea75d9dac04636774bc7ba1411c3df16bebae7ea
Tsuba-Nyan <tsu.sugi@gmail.com>
2024-02-14 13:30:08 +0000

    add: test tinydb

---
+++ python_template/Machine_Learning/3_2/tinydb-test.py
@@ -0,0 +1,31 @@
+from tinydb import TinyDB, Query
+
+# データベースに接続
+filepath = "test-tinydb.json"
+db = TinyDB(filepath)
+
+# 既存のデータがあれば破棄
+db.drop_table('fruits')
+
+# テーブルを得る
+table = db.table('fruits')
+
+# データをデータベースに挿入
+table.insert({'name': 'Banana', 'price': 600})
+table.insert({'name': 'Orange', 'price': 1200})
+table.insert({'name': 'Mango', 'price': 840})
+
+# 全データを抽出して表示
+print(table.all())
+
+# 特定のデータを抽出して表示
+# Orangeを検索
+Item = Query()
+res = table.search(Item.name == 'Orange')
+print('Orange is', res[0]['price'])
+
+# 値段が800円以上のものを抽出
+print("800円以上のもの：")
+res = table.search(Item.price >= 800)
+for it in res:
+    print("-", it['name'])

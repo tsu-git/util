ab707a61d1f0137f6446b42df281e079db52a14e
Tsuba-Nyan <tsu.sugi@gmail.com>
2024-06-16 17:09:18 +0900

    使用量／計器数の変換、電圧種別の変換。テストデータの修正。

--- python_template/operate_Excel_file/upd_delivery_list.py
+++ python_template/operate_Excel_file/upd_delivery_list.py
@@ -5,11 +5,50 @@
         - カラム区分
 
 """
-def search
 
 import sys
 from pathlib import Path
 import openpyxl
+import re
+
+
+def examine_filename_format(filename: str) -> bool:
+    if (matched := re.search(r'^[WM][LG][1-3][23]-', filename)):
+        return(True)
+    else:
+        return(False)
+
+
+def find_out_usage_and_meter(filename: str) -> str:
+    kind = ""
+
+    if filename[0] == "W":
+        kind = "使用量"
+    elif filename[0] == "M":
+        kind = "計器数"
+
+    return(kind)
+
+
+def find_out_voltage(filename: str) -> str:
+    kind = ""
+
+    if filename[2] == "1":
+        kind = "低圧"
+    elif filename[2] == "2":
+        kind = "高圧"
+    elif filename[2] == "3":
+        kind = "特高"
+
+    return(kind)
+
+
+# 定数定義
+HEADER_TARGET = "No."   # ヘッダ行のいずれかの項目
+SEARCH_RANGE_ROW_MAX = 50
+SEARCH_RANGE_COL_MAX = 30
+COL_IDX_FILENAME = 2
+COL_IDX_COLUMN_KBN = 3
 
 # 引数取得
 if len(sys.argv) < 2:
@@ -34,12 +73,51 @@ wb = openpyxl.load_workbook(list_file)
 # 対象のシートを選択する
 sheet = wb['Sheet1']
 
-# TODO: ヘッダ行を探す
+# ヘッダ行を探す
+for row_num in range(sheet.min_row, SEARCH_RANGE_ROW_MAX):
+    for col_num in range(sheet.min_column, SEARCH_RANGE_COL_MAX):
+        label = sheet.cell(row=row_num, column=col_num).value
+        if label == HEADER_TARGET:
+            match_found = True
+            break
+    if match_found:
+        break
+
+header_row = row_num
 
 # TODO: ファイル名から項目を自動計算する
+for row_num in range(header_row + 1, sheet.max_row):
+    filename = sheet.cell(row=row_num, column=COL_IDX_FILENAME).value
+
+    # ファイル名検査
+    if filename == None:
+        # ファイル名が空の場合終了
+        break
+    elif filename[-4:] != ".csv":
+        # csvファイル以外は無視
+        continue
+
+    print(filename, end=" ")
+
+    # 秘匿化前ファイルの場合はファイル名からキーワードを除去する
+    if filename[0:10] == "afterhide_":
+        filename = filename[10:]
+
+    if examine_filename_format(filename) != True:
+        continue
+
+    # 使用量／計器数
+    print(find_out_usage_and_meter(filename), end=" ")
+
+    # 電圧
+    print(find_out_voltage(filename))
+
+    # TODO: カラム区分
+
 
 # TODO: 納品一覧ファイルを保存
 
-# TODO: 納品一覧ファイルを閉じる
+# 納品一覧ファイルを閉じる
+wb.close()
 
 sys.exit()

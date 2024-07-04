a7f3f6440161379f07dbdb50cf330c0f15b87d67
Tsuba-Nyan <tsu.sugi@gmail.com>
2024-07-04 22:10:08 +0900

    make simpler to handle arguments

--- python_template/run_script/stub_script.py
+++ python_template/run_script/stub_script.py
@@ -1,19 +1,6 @@
 import sys, argparse, time
 
-# 引数を取得する
-parser = argparse.ArgumentParser(
-            prog='stub_script.py',
-            description='stub script for run_anotherScript.py'
-        )
-parser.add_argument('-f', '--file', help='configuration file')
-parser.add_argument('--tso_id')
-parser.add_argument('--ym', default="202401")
-parser.add_argument('--date', default="1")
-parser.add_argument('--voltage')
-args = parser.parse_args()
-
 time.sleep(1)
-print(f"received: {args.tso_id} {args.ym} {args.date}, "
-      f"voltage[{args.voltage}]")
+print(f"received: {argv[1:]}]")
 
 sys.exit()

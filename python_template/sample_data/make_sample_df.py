5aaccf5c79c5e169e5ecd74a5d6a9a493861e7c8
Tsuba-Nyan <tsu.sugi@gmail.com>
2024-11-03 17:23:37 +0900

    added a datetime column

--- python_template/sample_data/make_sample_df.py
+++ python_template/sample_data/make_sample_df.py
@@ -1,11 +1,16 @@
 import pandas as pd
 from random import randint, uniform, choice
+from datetime import datetime, timedelta
 
-# coding: utf-8
+start_dt = datetime.strptime('2024/11/3 00:00:00', '%Y/%m/%d %H:%M:%S')
 area_list = ['サンプル市', 'サンプル町', 'サンプル村']
-data = [{'area': f'{choice(area_list)}_{i}',
-         'value1': randint(5000, 6000), 
-         'value2': round(uniform(200, 2500), 2)}
+data = [
+        {
+            'area': f'{choice(area_list)}_{i}',
+            'datetime': f'{start_dt + timedelta(minutes=30)*i}',
+            'value1': randint(5000, 6000), 
+            'value2': round(uniform(200, 2500), 2)
+        }
             for i in range(20)]
 
 print(pd.DataFrame(data))

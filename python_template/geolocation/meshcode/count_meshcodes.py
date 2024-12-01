df19caa14b588d4647bcf12fb304bee936f7539a
Tsuba-Nyan <tsu.sugi@gmail.com>
2024-12-01 17:14:59 +0900

    add importing modules and creating a Path object

--- python_template/geolocation/meshcode/count_meshcodes.py
+++ python_template/geolocation/meshcode/count_meshcodes.py
@@ -1,4 +1,9 @@
 # coding: utf-8
+from pathlib import Path
+import pandas as pd
+
+p = Path("./")
+
 file_list = list(p.glob('[0-9][0-9].csv'))
 df = pd.concat([pd.read_csv(file, encoding="sjis", dtype='object') for file in file_list])
 df['pref_code'] = df['都道府県市区町村コード'].str[:2]
